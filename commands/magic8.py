5d592d69543eb2389664302d35f7d1837737cc6e
Zicklaa <phzi97@web.de>
2021-07-31 13:45:21 +0200

    Magic8 Antworten vergessen zu pushen lol

--- commands/magic8.py
+++ commands/magic8.py
@@ -7,7 +7,7 @@ logger = logging.getLogger("ZicklaaBot.Magic8")
 magic8_list = ["Zu 100% Ja!", "Es ist so entschieden, Ja.","Ja :3 OwO", "Ohne Zweifel, Ja.", "Definitiv, Ja.", "Kannst dich drauf verlassen, Digga.", "So wie ich des seh, scho, Ja.", "Denk scho, Ja.", "Ja.", "Ich hab Dieter um rat gefragt, er sagt Ja.",
                  "Merkur ist rückläufig, also Ja.", "Digga, bin hier grad Ballsdeep in etwas, frag später.", "Du würdest die Wahrheit nicht verkraften.", "Weiß nich, lol.", "Ich schwöre ich hab keine Ahnung von was du redest.", "Musst du Dieter fragen.", "Jesse, von was zum Fick redest du??",
                  "Nein.", "Nein, lol.", "Get fucked Digga, als ob ich dazu Ja sag.", "Du literarischer Untermensch, was stellst du mir so eine kernbehinderte Frage???", "Sieht nicht so aus.", "Würd ich ned drauf wetten.", "Niemals, Kollege.",
-                 "Dieter hat Nein gesagt :///", "Ich weiß nicht, aber wie wärs mit Megges?", "Nein, du HUNDT."]
+                 "Dieter hat Nein gesagt :///", "Ich weiß nicht, aber wie wärs mit Megges?", "Nein, du HUNDT.", "Geht dich nen scheißdreck an, Mois.", "H̶͕̦̬̮̖̼̠͈̗͈̤̥̣̣̋̂̐̆̈́̊͋́͠͝i̵̢̥̻̹͉̰̟̤͔̰̾͛ͅn̴̹͈̬̝̦͇̤͍͒̅ͅť̷̨̙̣̘̗͙̞͔̳͗̆͂̑e̸̢̫̝̭̗̰̙̲̟̼̝̺͇̥͜͝r̷̛̭̣͔͚̳͑̿̈́̄͋̂̊̾͗̉̾̿͜͝͝ ̸̨̬̖̦͉͍̯̜͕͔̪̭̜͚̒d̷̟̼̯̺͔̯̱̞̊̐̆̽̑̎̚͜ĭ̵̡͙̓̏͗̈̒͐͋̐̄͝ͅr̶̭̱̫͍͙͙̻̩͓̱̍̿̾́̓̈̆̔͂̂͒̏̽͝.", "Nur wenn KOSCHDELOS is."]
 
 class Magic8(commands.Cog):
     def __init__(self, bot):
