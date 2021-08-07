4b2157d888606f02419feeebe8cbd19f4a55db1d
Zicklaa <phzi97@web.de>
2021-08-08 00:59:51 +0200

    RAUL CRUISEHAUSEN KINDERMÖRDER

--- commands/kindermörder.py
+++ commands/kindermörder.py
@@ -13,9 +13,21 @@ class Kindermörder(commands.Cog):
     async def kindermörder(self, ctx):
         try:
             file = discord.File("raul.gif")
-            await ctx.reply(file=file, content="RAUL CRUISEHAUSEN KINDERMÖRDER")
-            '''await ctx.reply("RAUL CRUISEHAUSEN KINDERMÖRDER")
-            await ctx.channel.send("https://media.discordapp.net/attachments/122739462210846721/809326868989870110/ezgif-2-25ca1215409f.gif")'''
+            await ctx.channel.send(file=file, content="RAUL CRUISEHAUSEN KINDERMÖRDER")
+            await ctx.message.delete()
+            logger.info("Kindermörder gepostet für " + ctx.author.name)
+        except Exception as e:
+            await ctx.reply(
+                "Irgendwas klappt nedde. Scheiß Zicklaa zsamme gschwind. Hint: kindermörder()"
+            )
+            logger.error(f"Kindermörder from {ctx.author.name}: {e}")
+
+    @commands.command()
+    async def kühlschrank(self, ctx):
+        try:
+            file = discord.File("raul2.gif")
+            await ctx.channel.send(file=file)
+            await ctx.message.delete()
             logger.info("Kindermörder gepostet für " + ctx.author.name)
         except Exception as e:
             await ctx.reply(
