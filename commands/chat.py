52d8c55a7f3129bab2cbe45e63b7de880a3e5734
Zicklaa <phzi97@web.de>
2024-11-08 13:13:13 +0100

    hallo jack wenn du das ließt gib mir meine kinder zurück und das haus du hexe

--- commands/chat.py
+++ commands/chat.py
@@ -24,83 +24,11 @@ class Chat(commands.Cog):
         os.environ["FAL_KEY"] = self.bot.FAL_API_KEY
 
     @commands.command()
-    async def chat(self, ctx, *text):
+    async def chat(self, ctx, *, text):
         client_OAI = OpenAI(api_key=self.bot.OPENAI_API_KEY)
         if ctx.channel.id == 528742785935998979 or ctx.channel.id == 567411189336768532:
             async with ctx.channel.typing():
                 try:
-                    text = " ".join(text)
-                    text = text.replace('"', "")
-                    if text:
-                        completion = client_OAI.chat.completions.create(
-                            model="gpt-4o-mini",
-                            max_tokens=500,
-                            messages=[{"role": "user", "content": text}],
-                        )
-                        antwort = completion.choices[0].message.content
-                        kosten = completion.usage.total_tokens
-                        now = datetime.now()
-                        current_time = now.strftime("%H:%M:%S")
-                        if len(antwort) > 1023:
-                            if len(antwort) > 2000:
-                                await ctx.reply("Antwort leider zu lang für Discord :(")
-                                await ctx.reply(antwort)
-                                logger.info(
-                                    "Chat zu lang für " + ctx.author.name)
-                            else:
-                                embed = discord.Embed(
-                                    title="Antwort von ChatGPT auf den Prompt",
-                                    description=antwort,
-                                    color=0x00FF00,
-                                )
-                                embed.set_author(
-                                    name="ChatGPT",
-                                    icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/1024px-ChatGPT_logo.svg.png",
-                                )
-                                embed.set_footer(
-                                    text=str(current_time)
-                                    + " Uhr | Kosten: "
-                                    + str(kosten)
-                                    + " Tokens = "
-                                    + str(round(kosten * 0.00000015, 8))
-                                    + " Cent"
-                                )
-                                await ctx.reply(embed=embed)
-                                logger.info(
-                                    "Chat gepostet für " + ctx.author.name)
-                        else:
-                            embed = discord.Embed(
-                                title="Antwort von ChatGPT auf den Prompt",
-                                description="Prompt: " + text,
-                                color=0x00FF00,
-                            )
-                            embed.set_author(
-                                name="ChatGPT",
-                                icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/1024px-ChatGPT_logo.svg.png",
-                            )
-                            embed.add_field(
-                                name="Antwort", value=antwort, inline=False)
-                            embed.set_footer(
-                                text=str(current_time)
-                                + " Uhr | Kosten: "
-                                + str(kosten)
-                                + " Tokens = "
-                                + str(round(kosten * 0.00000015, 8))
-                                + " Cent"
-                            )
-                            await ctx.reply(embed=embed)
-                            logger.info(
-                                "Chat gepostet für " + ctx.author.name)
-                    else:
-                        await ctx.reply("Brauch schon nen Text von dir du Lellek.")
-                        logger.info("Chat kein Text von " +
-                                    ctx.author.name)
-                except Exception as e:
-                    await ctx.reply(e.message)
-                    logger.error(f"Chat Error from {ctx.author.name}: {e}")
-                try:
-                    text = " ".join(text)
-                    text = text.replace('"', "")
                     if text:
                         completion = client_OAI.chat.completions.create(
                             model="gpt-4o-mini",
@@ -173,7 +101,7 @@ class Chat(commands.Cog):
             logger.info(f"Chat außerhalb Meme Channel von {ctx.author.name}")
 
     @commands.command()
-    async def hmchat(self, ctx, *text):
+    async def hmchat(self, ctx, *, text):
         client_OAI = OpenAI(api_key=self.bot.OPENAI_API_KEY)
         if ctx.channel.id == 528742785935998979 or ctx.channel.id == 567411189336768532:
             async with ctx.channel.typing():
@@ -258,93 +186,15 @@ class Chat(commands.Cog):
                                     f"Chat Error from {ctx.author.name}: {e}")
                                 attempts += 1
                     break
-                    # satz = json_model.make_short_sentence(140)
-                    text = self.json_model.make_sentence(
-                        max_overlap_ratio=0.65,
-                    )
-                    if text:
-                        while attempts < max_attempts:
-                            try:
-                                preprompt = "Du bist Discordnutzer des Discord-Servers >Bens Haus der Enten<. Versuche unter allen umständen auf den folgenden Text zu antworten, auch wenn du ihn vielleicht nicht verstehst. Benutze wenn möglich anglizismen und generelle GEN Z Slang Wörter. Erwähne nicht dass du ein LLM bist oder dass du den Text nicht verstehst. Antworte wie ein cooler Jugendlicher und benutze wenn es passt auch Emojis. Gehe dabei auf alle Elemente des Texts ein. Baue so viel wissen und referenzen ein wie du nur kannst. Der Satz auf den du antworten sollst kommt jetzt:"
-
-                                completion = client_OAI.chat.completions.create(
-                                    model="gpt-4o-mini",
-                                    max_tokens=500,
-                                    messages=[
-                                        {"role": "user", "content": preprompt + text}],
-                                )
-                                antwort = completion.choices[0].message.content
-                                kosten = completion.usage.total_tokens
-                                now = datetime.now()
-                                current_time = now.strftime("%H:%M:%S")
-                                if len(antwort) > 1023:
-                                    if len(antwort) > 2000:
-                                        await ctx.reply("Antwort leider zu lang für Discord :(")
-                                        logger.info(
-                                            "Chat zu lang für " + ctx.author.name)
-                                    else:
-                                        embed = discord.Embed(
-                                            title="Antwort von ChatGPT auf den Prompt",
-                                            description=antwort,
-                                            color=0x00FF00,
-                                        )
-                                        embed.set_author(
-                                            name="ChatGPT",
-                                            icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/1024px-ChatGPT_logo.svg.png",
-                                        )
-                                        embed.set_footer(
-                                            text=str(current_time)
-                                            + " Uhr | Kosten: "
-                                            + str(kosten)
-                                            + " Tokens = "
-                                            + str(round(kosten * 0.00000015, 8))
-                                            + " Cent"
-                                        )
-                                        await ctx.reply(text)
-                                        await ctx.reply(embed=embed)
-                                        logger.info(
-                                            "Chat gepostet für " + ctx.author.name)
-                                else:
-                                    embed = discord.Embed(
-                                        title="Antwort von ChatGPT auf den Prompt",
-                                        description="Prompt: " + text,
-                                        color=0x00FF00,
-                                    )
-                                    embed.set_author(
-                                        name="ChatGPT",
-                                        icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/1024px-ChatGPT_logo.svg.png",
-                                    )
-                                    embed.add_field(
-                                        name="Antwort", value=antwort, inline=False)
-                                    embed.set_footer(
-                                        text=str(current_time)
-                                        + " Uhr | Kosten: "
-                                        + str(kosten)
-                                        + " Tokens = "
-                                        + str(round(kosten * 0.00000015, 8))
-                                        + " Cent"
-                                    )
-                                    await ctx.reply(text)
-                                    await ctx.reply(embed=embed)
-                                    logger.info(
-                                        "Chat gepostet für " + ctx.author.name)
-                                attempts = 999999
-                            except Exception as e:
-                                await ctx.reply("Irgendwas klappt nit lel.")
-                                logger.error(
-                                    f"Chat Error from {ctx.author.name}: {e}")
-                                attempts += 1
-                    break
         else:
             await ctx.reply("Spam woanders, Moruk 🤷")
             logger.info(f"Chat außerhalb Meme Channel von {ctx.author.name}")
 
     @commands.command()
-    async def bild(self, ctx, *text):
+    async def bild(self, ctx, *, text):
         if ctx.channel.id == 528742785935998979 or ctx.channel.id == 567411189336768532:
             async with ctx.channel.typing():
                 try:
-                    text = " ".join(text)
                     if text:
                         await self.getImage(ctx, text, "image.jpg", "true", "schnell")
                         logger.info("Bild gepostet für " + ctx.author.name)
@@ -359,11 +209,10 @@ class Chat(commands.Cog):
             logger.info(f"Bild außerhalb Meme Channel von {ctx.author.name}")
 
     @commands.command()
-    async def nsfw(self, ctx, *text):
+    async def nsfw(self, ctx, *, text):
         if ctx.channel.id == 528742785935998979 or ctx.channel.id == 567411189336768532:
             async with ctx.channel.typing():
                 try:
-                    text = " ".join(text)
                     if text:
                         await self.getImage(ctx, text, "SPOILER_image.jpg", "false", "schnell")
                         logger.info("Porn gepostet für " + ctx.author.name)
@@ -378,11 +227,10 @@ class Chat(commands.Cog):
             logger.info(f"Porn außerhalb Meme Channel von {ctx.author.name}")
 
     @commands.command()
-    async def hd(self, ctx, *text):
+    async def hd(self, ctx, *, text):
         if ctx.channel.id == 528742785935998979 or ctx.channel.id == 567411189336768532:
             async with ctx.channel.typing():
                 try:
-                    text = " ".join(text)
                     if text:
                         await self.getImage(ctx, text, "image.jpg", "true", "dev")
                         logger.info("HD gepostet für " + ctx.author.name)
@@ -397,11 +245,10 @@ class Chat(commands.Cog):
             logger.info(f"HD außerhalb Meme Channel von {ctx.author.name}")
 
     @commands.command()
-    async def hdnsfw(self, ctx, *text):
+    async def hdnsfw(self, ctx, *, text):
         if ctx.channel.id == 528742785935998979 or ctx.channel.id == 567411189336768532:
             async with ctx.channel.typing():
                 try:
-                    text = " ".join(text)
                     if text:
                         await self.getImage(ctx, text, "SPOILER_image.jpg", "false", "dev")
                         logger.info("HDNSFW gepostet für " + ctx.author.name)
@@ -416,7 +263,7 @@ class Chat(commands.Cog):
             logger.info(f"HDNSFW außerhalb Meme Channel von {ctx.author.name}")
 
     @commands.command()
-    async def pipeline(self, ctx, *text):
+    async def pipeline(self, ctx, *, text):
         client_OAI = OpenAI(api_key=self.bot.OPENAI_API_KEY)
 
         preprompt = "You will be given user input. Given the user's input message, create a detailed, vivid description to generate a high-quality image using AI. Include specific details about the scene, background, objects, and emotions. Consider the lighting, colors, textures, and any relevant environment or mood. Ensure that the description is coherent and descriptive enough to guide the AI in generating an accurate and visually appealing image. But also keep yourself short and precise as not to overload the image generator with too much input. The output should be a single, comprehensive prompt that fully captures the essence of the user's input. Return only the output. The output has to be in english. Input: "
@@ -424,8 +271,6 @@ class Chat(commands.Cog):
         if ctx.channel.id == 528742785935998979 or ctx.channel.id == 567411189336768532:
             async with ctx.channel.typing():
                 try:
-                    text = " ".join(text)
-                    text = text.replace('"', "")
                     if text:
                         completion = client_OAI.chat.completions.create(
                             model="gpt-4o-mini",
@@ -436,7 +281,7 @@ class Chat(commands.Cog):
                         antwort = completion.choices[0].message.content
 
                         if antwort:
-                            await self.getImage(ctx, antwort, "SPOILER_image.jpg", "false", "dev")
+                            await self.getImage(ctx, antwort, "SPOILER_image.jpg", "false", "pipeline")
                             logger.info(
                                 "HDNSFW Pipeline gepostet für " + ctx.author.name)
                         else:
@@ -457,6 +302,9 @@ class Chat(commands.Cog):
     async def getImage(self, ctx, text, imageName, blockNsfw, model):
         if (model == "schnell"):
             num_inference_steps = 2
+        elif (model == "pipeline"):
+            model = "schnell"
+            num_inference_steps = 4
         else:
             num_inference_steps = 28
 
@@ -474,73 +322,25 @@ class Chat(commands.Cog):
         response.raise_for_status()
         await ctx.reply(file=discord.File(BytesIO(response.content), imageName))
 
-    '''
     @commands.command()
-    async def bildAlt(self, ctx, *text):
-        client_OAI = OpenAI(api_key=self.bot.OPENAI_API_KEY)
-        if ctx.channel.id == 528742785935998979 or ctx.channel.id == 567411189336768532:
-            async with ctx.channel.typing():
-                try:
-                    text = " ".join(text)
-                    text = text.replace('"', "")
-                    if text:
-                        completion = client_OAI.images.generate(
-                            model="dall-e-3",
-                            n=1,
-                            size="1024x1024",
-                            prompt=text,
-                            quality="standard"
-                        )
-                        antwort = completion.data[0].url
-
-                        response = requests.get(antwort)
-                        response.raise_for_status()
-                        await ctx.reply(file=discord.File(BytesIO(response.content), 'image.jpg'))
-                        logger.info("Bild gepostet für " + ctx.author.name)
-                    else:
-                        await ctx.reply("Brauch schon nen Text von dir du Lellek.")
-                        logger.info("Bild kein Text von " + ctx.author.name)
-                except Exception as e:
-                    await ctx.reply("Fehler lul")
-                    logger.error(f"Bild Error from {ctx.author.name}: {e}")
-        else:
-            await ctx.reply("Spam woanders, Moruk 🤷")
-            logger.info(f"Bild außerhalb Meme Channel von {ctx.author.name}")
-        '''
-        '''
-
-    @commands.command()
-    async def tts(self, ctx, *text):
+    async def tts(self, ctx, *, text):
         client_OAI = OpenAI(api_key=self.bot.OPENAI_API_KEY)
         names = ["alloy", "echo", "fable", "onyx", "nova", "shimmer"]
 
-        names = ["alloy", "echo", "fable", "onyx", "nova", "shimmer"]
+        first_word = get_first_word(text)
 
         async with ctx.channel.typing():
             try:
-                text = " ".join(text)
-                text = text.replace('"', "")
-
-                first_word_in_list = text.split()[0] in names
-
-                if not first_word_in_list:
-                    voice = "onyx"
+                if (first_word in names):
+                    voice = first_word
+                    text = text.replace(first_word, "")
                 else:
-                    (voice, text) = text.split(maxsplit=1)
-
-                first_word_in_list = text.split()[0] in names
-
-                if not first_word_in_list:
                     voice = "onyx"
-                else:
-                    (voice, text) = text.split(maxsplit=1)
                 if text:
                     completion = client_OAI.audio.speech.create(
                         model="tts-1-hd",
                         voice=voice,
-                        voice=voice,
                         input=text
-                    )
                     )
                     mp3_file = BytesIO(completion.content)
                     mp3_file.seek(0)
@@ -552,6 +352,14 @@ class Chat(commands.Cog):
             except Exception as e:
                 await ctx.reply("Fehler lul")
                 logger.error(f"TTS Error from {ctx.author.name}: {e}")
+
+
+def get_first_word(string):
+    words = string.split()
+    if words:
+        return words[0]
+    else:
+        return None  # Return None if the string is empty
 
 
 def setup(bot):
